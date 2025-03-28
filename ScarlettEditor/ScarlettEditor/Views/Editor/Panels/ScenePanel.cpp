fdaf46ed91f7663c11239fee57e37cda19a811c5
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2025-03-28 16:19:00 +0000

    Refactored scene graph to use a tree and show the selected entity.

--- ScarlettEditor/ScarlettEditor/Views/Editor/Panels/ScenePanel.cpp
+++ ScarlettEditor/ScarlettEditor/Views/Editor/Panels/ScenePanel.cpp
@@ -6,7 +6,6 @@
 #include <ScarlettGameCore/Src/GameCore.h>
 #include <ScarlettGameCore/Components/Tag.h>
 
-#include "Editor/EditorManager.h"
 #include "Views/Editor/View/EditorView.h"
 
 namespace ScarlettEditor
@@ -14,29 +13,64 @@ namespace ScarlettEditor
 
 void ScenePanel::Render()
 {
+    EditorView* editorView = dynamic_cast<EditorView*>(mView);
+
     ScarlEntt::ComponentManager* componentManager = ScarlettGame::GameCore::Instance().GetActiveScene()->GetComponentManager();
     auto& tags = componentManager->GetComponentArray<ScarlettGame::Tag>();
 
-    if (ImGui::Button("Add Entity"))
-    {
-        auto ent = ScarlettGame::GameCore::Instance().CreateEntity();
-    }
+    // ----------- Title Bar ----------------
+    ImGui::BeginGroup();
 
-    bool selectedEntity = false;
+    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 5.0f);
+    ImGui::Text("Default Scene Name");
+    ImGui::SameLine();
+    ImGui::SetCursorPos({ ImGui::GetWindowWidth()  - 35.0f, ImGui::GetCursorPosY() - 5.0f });
+    if (ImGui::Button("+"))
+    {
+        (void)ScarlettGame::GameCore::Instance().CreateEntity();
+    }
+    ImGui::EndGroup();
+
+    ImGui::Separator();
+
+    // ------------- Tree -------------------
+    const float indentSpacing = ImGui::GetStyle().IndentSpacing;
+    ImGui::GetStyle().IndentSpacing = 0.0f;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(10, 10));
+    ImGui::TreePush("Scene");
+
     for (ScarlEntt::ComponentId i {0}; i < tags.Size(); ++i)
     {
-        if (ImGui::CollapsingHeader(tags[i].name.c_str()))
-        {
-            selectedEntity = true;
+        ImGuiTreeNodeFlags nodeFlags = ImGuiTreeNodeFlags_SpanAvailWidth;
 
-        	dynamic_cast<EditorView*>(mView)->GetSelectionManager().SetSelectedEntity(&tags[i].entity);
+        if (editorView->GetSelectionManager().GetSelectedEntity() == &tags[i].entity)
+        {
+            nodeFlags |= ImGuiTreeNodeFlags_Selected;
+        }
+
+        // todo when there are child entities, enable this and have the scene graph show the children indented.
+        constexpr bool hasChildEntity = false;
+        if (!hasChildEntity)
+        {
+            nodeFlags |= ImGuiTreeNodeFlags_Leaf;
+        }
+
+        if (ImGui::TreeNodeEx(tags[i].name.c_str(), nodeFlags))
+        {
+            if (ImGui::IsItemClicked())
+            {
+                editorView->GetSelectionManager().SetSelectedEntity(&tags[i].entity);
+            }
+
+            ImGui::TreePop();
         }
     }
 
-    if (!selectedEntity)
-    {
-        dynamic_cast<EditorView*>(mView)->GetSelectionManager().SetSelectedEntity(nullptr);
-    }
+    ImGui::TreePop();
+    ImGui::PopStyleVar();
+
+    ImGui::GetStyle().IndentSpacing     = indentSpacing;
 }
 
 } // Namespace ScarlettEditor
